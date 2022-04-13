785713e026abb0f414f9867a544b3131253caae9
SwapnaG01 <SwapnaG01>
2022-04-13 09:22:38 +0100

    updated cart page as per swapna's review

--- src/main/java/com/qa/operncart/pages/CartPage.java
+++ src/main/java/com/qa/operncart/pages/CartPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class CartPage {
 	
-	private By cart=By.id("cart");
+	private By cart1=By.id("cart");
 	public void addCart()
 	{
 		System.out.println("add to cart is done");
