9e057bf31aaf057b9a898a7b30c31c58b60254a7
SwapnaG01 <SwapnaG01>
2022-04-13 08:26:01 +0100

    added cartpage

---
+++ src/main/java/com/qa/operncart/pages/CartPage.java
@@ -0,0 +1,13 @@
+package com.qa.operncart.pages;
+
+public class CartPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=23;
+		System.out.println(i);
+
+	}
+
+}
