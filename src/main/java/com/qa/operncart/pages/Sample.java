e08d89b2dc0b5cb86a7aee16c595b1b8bb2818ed
SwapnaG01 <103461892+SwapnaG01@users.noreply.github.com>
2022-04-17 23:28:28 +0100

    Create Sample.java

---
+++ src/main/java/com/qa/operncart/pages/Sample.java
@@ -0,0 +1,14 @@
+package com.qa.operncart.pages;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String name="Tom";
+    String password="Tom123";
+
+	}
+
+}
