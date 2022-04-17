ee979dce1fce37edf594368af62a092677b61c81
SwapnaG01 <103461892+SwapnaG01@users.noreply.github.com>
2022-04-18 00:05:53 +0100

    Create OrderPage.java

---
+++ src/main/java/com/qa/operncart/pages/OrderPage.java
@@ -0,0 +1,21 @@
+package com.qa.operncart.pages;
+
+public class OrderPage {
+	
+  public void getOrderId()
+	{
+		System.out.println("order id 1234...");
+	}
+  
+	
+	public void makeorder()
+	{
+		System.out.println("order page...");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
