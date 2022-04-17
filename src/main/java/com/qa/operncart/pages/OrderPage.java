5c1fe4994098a1765851bb5e2d1dea84c452eaf7
SwapnaG01 <SwapnaG01>
2022-04-18 00:21:42 +0100

    added orderpage after conflict

--- src/main/java/com/qa/operncart/pages/OrderPage.java
+++ src/main/java/com/qa/operncart/pages/OrderPage.java
@@ -2,15 +2,19 @@ package com.qa.operncart.pages;
 
 public class OrderPage {
 	
+
   public void getOrderId()
 	{
 		System.out.println("order id 1234...");
 	}
   
-	
-	public void makeorder()
+
+   public void makeorder()
 	{
 		System.out.println("order page...");
+        System.out.println(" order is generated..");
+  
+        
 	}
 
 	public static void main(String[] args) {
