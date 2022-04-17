b01969f40277548c9c9e5a4cacaf63029d96bb2a
SwapnaG01 <SwapnaG01>
2022-04-17 23:53:47 +0100

    this is sample naveen code

---
+++ src/main/java/com/qa/operncart/pages/Sample.java
@@ -0,0 +1,16 @@
+package com.qa.operncart.pages;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		String username="Naveen";
+		
+		
+		
+		
+		
+	}
+
+}
