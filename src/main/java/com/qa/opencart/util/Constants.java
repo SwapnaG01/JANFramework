afb8f83655ef617f82dadec8537fb81aa08cd7ca
SwapnaG01 <SwapnaG01>
2022-04-23 08:47:10 +0100

    modified four files

--- src/main/java/com/qa/opencart/util/Constants.java
+++ src/main/java/com/qa/opencart/util/Constants.java
@@ -8,10 +8,10 @@ public class Constants {
 	public static final int DEFAULT_TIME_OUT=5;
 	public static final String DEMO_PAGE_TITLE="Account DEMO";
 	public static final String LOGIN_PAGE_TITLE="Account Login";
-	public static final String LOGIN_PAGE_FRACTION_URL ="route=account/login";
+	public static final String LOGIN_PAGE_FRACTION_URL ="http://opencart.antropy.co.uk/index.php?route=account/login";
 	public static final String ACCOUNTS_PAGE_TITLE ="My Account";
     
-	public static final List<String> ACCOUNTS_SECTIONS_LIST=Arrays.asList("My Account","My Orders","My Affiliate Account","Newsletter");
+	public static final List<String> ACCOUNTS_SECTIONS_LIST=Arrays.asList("My Account","My Orders","Newsletter");
 
 	public static final int MACBOOK_PRODUCT_COUNT =3;
 	public static final int IMAC_PRODUCT_COUNT =1;
